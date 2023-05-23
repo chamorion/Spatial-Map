cd0ff4b04e57e04e38b8f4e89463ef86b247ab6f
Chamorion <12010822@mail.sustech.edu.cn>
2023-05-24 02:57:52 +0800

    spatial smooth

--- +lib/Rate_Matrix.m
+++ +lib/Rate_Matrix.m
@@ -15,7 +15,7 @@ classdef Rate_Matrix < handle
     methods
         function rm = Rate_Matrix(cell_id, time, x, y, spk_time)
             rm.cell_id = cell_id;
-            [rm.time, rm.x, rm.y, rm.spk_time] = lib.Rate_Matrix.std_clean(time, x, y, spk_time);
+            [rm.time, rm.x, rm.y, rm.spk_time] = lib.Rate_Matrix.std_preprocess(time, x, y, spk_time);
             
             % interpolant function of xy coordinates vs time
             rm.itp_x=griddedInterpolant(rm.time,rm.x);
@@ -30,7 +30,32 @@ classdef Rate_Matrix < handle
             hm_xy(hm_xy==0)=NaN;
             hm_time=hm_xy*delta_t;
             hm_spk=histcounts2(rm.spk_x,rm.spk_y,'XBinEdges',x_edge,'YBinEdges',y_edge);
-            rm.rate_map=hm_spk./hm_time;
+            rm.rate_map=flipud(rot90(hm_spk./hm_time));
+        end              
+        
+        % spatial smoothing based on Gaussian kernel
+        function rm = spatial_smooth(rm, bin_num)
+            delta_x = (max(rm.x) - min(rm.x))/bin_num;
+            delta_y = (max(rm.y) - min(rm.y))/bin_num;
+            delta_t = mean(diff(rm.time));
+            sigma = min([delta_x, delta_y])/3;
+            Gauss_K = @(v1, v2) exp(- (v1.^2 + v2.^2)/2*sigma^2);
+            
+            p_x = @(t) rm.itp_x(t);
+            p_y = @(t) rm.itp_y(t);
+
+            rm.rate_map = zeros(bin_num, bin_num);
+            i_x = 1;
+            for s_x = min(rm.x) + delta_x/2 : delta_x : max(rm.x) - delta_x/2
+                i_y = 1;
+                for s_y = min(rm.y) + delta_y/2 : delta_y : max(rm.y) - delta_y/2
+                    rm.rate_map(i_x, i_y) = sum(Gauss_K(rm.spk_x - s_x, rm.spk_y - s_y))/sum(Gauss_K(p_x(rm.time) - s_x, p_y(rm.time) - s_y)*delta_t);
+                    i_y = i_y + 1;
+                end
+                i_x = i_x + 1;
+            end
+            hm_xy=histcounts2(rm.x,rm.y,'NumBins',[bin_num, bin_num]);
+            rm.rate_map(flipud(rot90(hm_xy)) == 0)=NaN;
         end
         
         % plot trajactory of the cell
@@ -49,7 +74,7 @@ classdef Rate_Matrix < handle
             figure;
             hold on;
             title('place field');
-            hm=imagesc(flipud(rot90(rm.rate_map)));
+            hm=imagesc(rm.rate_map);
             set(gca,'YDir','normal');
             colormap('turbo');
             set(hm,'AlphaData',~isnan(hm.CData));
@@ -58,10 +83,10 @@ classdef Rate_Matrix < handle
     end
     
     methods (Static)
-        % standard data cleaning
-        function [time, x, y, spk_time] = std_clean(time, x, y, spk_time)
+        % standard data preprocessing: data cleaning and centering
+        function [time, x, y, spk_time] = std_preprocess(time, x, y, spk_time)
             % drop nan value
-            vld = (logical(1-isnan(x))) & (logical(1-isnan(y))) & (logical(1-isnan(time)));
+            vld = (logical(1-isnan(x))) & (logical(1-isnan(y)));
             x = x(vld);
             y = y(vld);
             time = time(vld);
@@ -73,13 +98,20 @@ classdef Rate_Matrix < handle
             x = x(vld);
             y = y(vld);
             time = time(vld);
+            
+            % x,y centering
             x = x - min(x);
             y = y - min(y);
-
+            
             % drop invalid time of spikes
             vld = logical(spk_time <= max(time)) & logical(spk_time >= min(time));
             spk_time = spk_time(vld);
+            
+            % time centering
+            spk_time = spk_time - min(time);
+            time = time - min(time);
         end
+        
     end
     
 end
